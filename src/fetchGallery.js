f252abf82e5978151f6cff8d8452ad7136671949
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-05-12 15:11:28 +0200

    fetch working, gallery not working

--- src/fetchGallery.js
+++ src/fetchGallery.js
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
-async function getGallery() {
-  try {
-    const response = await axios.get('/user?ID=12345');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
+const API_URL = 'https://pixabay.com/api/';
+const params = new URLSearchParams({
+  key: '30372139-17e231f0453b63e760e856e5b',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 1,
+  per_page: 40,
+});
+
+export const fetchGallery = async query => {
+  const response = await axios.get(`${API_URL}?${params}&q=${query}`);
+  const data = await response.data;
+  console.log('this is response >>', response);
+  console.log('this is data >>', data);
+
+  return data;
+};
+
+// https:pixabay.com/api/?key=30372139-17e231f0453b63e760e856e5b&q=yellow+flowers&image_type=photo
--- src/index.js
+++ src/index.js
@@ -2,52 +2,60 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import photos from './pexelsTest';
+import { fetchGallery } from './fetchGallery';
 
 const searchForm = document.getElementById('search-form');
 const searchInput = document.querySelector('.search-input');
 const searchBtn = document.querySelector('.search-submit');
 const galleryEl = document.getElementById('gallery');
 
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = searchInput.value;
+  if (!searchInput.value.trim()) {
+    galleryEl.innerHTML = '';
+  } else {
+    galleryEl.innerHTML = '';
+    fetchGallery(query)
+      .then(data => {
+        if (data.total === 0) {
+          Notiflix.Notify.failure(`Sorry, we couldn't find anything :(`);
+          return;
+        }
+        // lightbox.refresh();
+        Notiflix.Notify.success(`We found ${data.totalHits} hits`);
+        galleryCompleter(data);
+      })
+      .catch(error => console.log(error));
+  }
+});
+
 function galleryCompleter(data) {
-  data.forEach(data => {
+  const photo = data.hits;
+  photo.forEach(photo => {
     galleryEl.innerHTML += `
-      <div class="photo-card"><a href="${data.previewURL}" class="gallery-link">
-        <img src="${data.previewURL}" class="gallery-image" alt="${data.tags}" loading="lazy" />
+      <div class="photo-card"><a href="${photo.largeImageURL}" class="gallery-link">
+        <img src="${photo.webformatURL}" class="gallery-image" alt="${photo.tags}" loading="lazy" />
         </a>
         <div class="info">
           <p class="info-item">
             <b>Likes</b>
-            ${data.likes}
+            ${photo.likes}
           </p>
           <p class="info-item">
             <b>Views</b>
-            ${data.views}
+            ${photo.views}
           </p>
           <p class="info-item">
             <b>Comments</b>
-            ${data.comments}
+            ${photo.comments}
           </p>
           <p class="info-item">
             <b>Downloads</b>
-            ${data.downloads}
+            ${photo.downloads}
           </p>
         </div>
       </div>`;
   });
 }
-
-
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
-
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (searchInput.value === "") {
-    galleryEl.innerHTML = '';
-  } else {
-    galleryCompleter(photos.hits);
-    lightbox.refresh()
-  }
-})
+const lightbox = new SimpleLightbox('.gallery a');
